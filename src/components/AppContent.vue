55075e05846a413847523790e2f1de6a84d80a74
hajjamn <hajjamn@gmail.com>
2024-07-31 11:32:23 +0200

    FIX: added env

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,31 +1,31 @@
-<script>
-
-export default {
-  components: {
-  },
-  data() {
-    return {
-    }
-  }
-}
-
-</script>
-
 <template>
-
   <main>
     <section class="h-100">
       <div class="container py-5 h-100">
         <div class="row aling-items-center flex-column justify-content-center h-100">
           <div class="col-auto text-center">
             <h1>Hello Vue <font-awesome-icon :icon="['fab', 'vuejs']" /></h1>
-            <button class="btn btn-primary" @click="console.log(mongodbUri)"></button>
+            <button class="btn btn-primary" @click="logMongoDBUri">Log MongoDB URI</button>
           </div>
         </div>
       </div>
     </section>
   </main>
-
 </template>
+
+<script>
+export default {
+  name: 'AppContent',
+  inject: ['mongodbUri'],
+  data() {
+    return {};
+  },
+  methods: {
+    logMongoDBUri() {
+      console.log(this.mongodbUri);
+    }
+  }
+}
+</script>
 
 <style></style>
