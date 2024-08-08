a4bae030a121c8896396e1a4202d4c7f92795aa1
Jasczlv <ianis.cozlov@gmail.com>
2024-08-08 16:55:31 +0200

    add lorem

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,14 +1,15 @@
 <script>
 export default {
-  name: 'AppContent',
+  name: "AppContent",
   data() {
     return {
-      responseData: Object
+      responseData: Object,
     };
   },
   methods: {
     test() {
-      this.$axios.get('/fetch-travel-app-data')
+      this.$axios
+        .get("/fetch-travel-app-data")
         .then((response) => {
           console.log(response.data);
         })
@@ -23,18 +24,26 @@ export default {
       .catch((error) => {
         console.log(error);
       }); */
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
   <main>
     <section class="h-100">
       <div class="container py-5 h-100">
-        <div class="row aling-items-center flex-column justify-content-center h-100">
+        <div
+          class="row aling-items-center flex-column justify-content-center h-100"
+        >
           <div class="col-auto text-center">
             <h1>Hello Vue <font-awesome-icon :icon="['fab', 'vuejs']" /></h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+              fugiat dolore architecto libero? Praesentium nisi sit enim
+              dolorum, aperiam iusto ab doloribus. Fugit non sit suscipit sunt
+              obcaecati laboriosam perspiciatis?
+            </p>
             <button class="btn btn-warning" @click="test">Test</button>
           </div>
         </div>
@@ -42,6 +51,5 @@ export default {
     </section>
   </main>
 </template>
-
 
 <style></style>
