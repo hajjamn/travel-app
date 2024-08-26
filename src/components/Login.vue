823701222b291de423d3c9f93b20907f663f3ca3
Jasczlv <ianis.cozlov@gmail.com>
2024-08-26 05:21:27 +0200

    ADD sistemando login su vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,6 +13,27 @@ export default {
       showErrorAlert: false,
     };
   },
+  created() {
+    this.$axios
+      .get("fetch-token")
+      .then((response) => {
+        // Print response in the console
+        console.log(response);
+        this.userLog.username = response.data.user.name;
+        if (response.data.message === "Login successful") {
+          // Navigate to another page, e.g., a dashboard
+          // this.$router.push("/dashboard");
+        }
+      })
+      .catch((error) => {
+        // Show error alert on UI
+        this.showErrorAlert = error.response.data.showErrorAlert;
+        console.error(
+          "Error:",
+          error.response ? error.response.data : error.message
+        );
+      });
+  },
   methods: {
     query(dataUser) {
       this.$axios
@@ -21,31 +42,31 @@ export default {
         })
         .then((response) => {
           // Print response in the console
-          console.log(response.headers);
+          // console.log(response.headers);
           if (response.data.message === "Login successful") {
+            this.$axios
+              .get("fetch-token")
+              .then((response) => {
+                // Print response in the console
+                // console.log(response);
+                this.userLog.username = response.data.user.name;
+                if (response.data.message === "Login successful") {
+                  // Navigate to another page, e.g., a dashboard
+                  // this.$router.push("/dashboard");
+                  location.reload();
+                }
+              })
+              .catch((error) => {
+                // Show error alert on UI
+                this.showErrorAlert = error.response.data.showErrorAlert;
+                console.error(
+                  "Error:",
+                  error.response ? error.response.data : error.message
+                );
+              });
+            // location.reload();
             // Navigate to another page, e.g., a dashboard
-            this.$router.push("/dashboard");
-          }
-        })
-        .catch((error) => {
-          // Show error alert on UI
-          this.showErrorAlert = error.response.data.showErrorAlert;
-          console.error(
-            "Error:",
-            error.response ? error.response.data : error.message
-          );
-        });
-    },
-    login() {
-      this.$axios
-        .get("fetch-token")
-        .then((response) => {
-          // Print response in the console
-          console.log(response);
-          this.userLog.username = response.data.user.name;
-          if (response.data.message === "Login successful") {
-            // Navigate to another page, e.g., a dashboard
-            this.$router.push("/dashboard");
+            // this.$router.push("/dashboard");
           }
         })
         .catch((error) => {
@@ -61,25 +82,28 @@ export default {
 };
 </script>
 <template>
-  <h1>Login</h1>
-  <h2 v-if="showErrorAlert">ERRORE</h2>
-  <form @submit.prevent="query(user)">
-    <label for="email">Your email</label>
-    <input
-      type="email"
-      id="email"
-      v-model="user.email"
-      placeholder="your@email.com"
-    /><br />
-    <label for="password">Password</label>
-    <input type="password" id="password" v-model="user.password" /><br />
-    <input type="submit" value="Submit" />
-  </form>
-  <div>
-    <button @click="query(user)">Test</button>
+  <div v-if="userLog.username === ''">
+    <h1>Login</h1>
+    <h2 v-if="showErrorAlert">ERRORE</h2>
+    <form @submit.prevent="query(user)">
+      <label for="email">Your email</label>
+      <input
+        type="email"
+        id="email"
+        v-model="user.email"
+        placeholder="your@email.com"
+      /><br />
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="user.password" /><br />
+      <input type="submit" value="Submit" />
+    </form>
   </div>
-  <button @click="login()">Login test</button>
-  <h1>{{ userLog.username }}</h1>
+  <!-- <div>
+    <button @click="query(user)">Test</button>
+  </div> -->
+
+  <h1>your username: {{ userLog.username }}</h1>
+  <button>Logout da fare</button>
 </template>
 
 <style lang="scss" scoped></style>
