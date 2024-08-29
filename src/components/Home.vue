2e27f21591c1ea6a71cbbbc7346389ce2648e844
hajjamn <hajjamn@gmail.com>
2024-08-29 17:55:13 +0200

    ADD: ordered travels

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,9 @@ export default {
   data() {
     return {
       responseData: {}, // Initialize as an empty object
-      today: new Date()
+      today: new Date(),
+      futureTravels: [],
+      pastTravels: [],
     };
   },
   methods: {
@@ -14,11 +16,19 @@ export default {
       this.$axios
         .get("/fetch-travel-app-data")
         .then((response) => {
-          // Assuming the date format in the response is 'YYYY-MM-DD'
           this.responseData = response.data.data;
+          // Separate travels into future and past
+          console.log(this.responseData.travels)
           this.responseData.travels.forEach(travel => {
             const startDate = new Date(travel.start_date);
             travel.daysUntilStart = Math.ceil((startDate - this.today) / (1000 * 60 * 60 * 24));
+
+            // Classify the travel based on its start date
+            if (startDate >= this.today) {
+              this.futureTravels.push(travel);
+            } else {
+              this.pastTravels.push(travel);
+            }
           });
         })
         .catch((error) => {
@@ -95,12 +105,13 @@ export default {
       <div class="container py-5 h-100">
         <div class="row align-items-center flex-column justify-content-between h-100">
           <div class="col-auto text-center">
+          </div>
+          <!-- Future Travels Section -->
+          <div v-if="futureTravels" class="row text-center">
             <h3 class="text-start align-self-start">
               Here is your planned vacay:
             </h3>
-          </div>
-          <div class="row text-center">
-            <div class="col-12 mb-3" v-for="travel in responseData.travels" :key="travel._id">
+            <div class="col-12 mb-3" v-for="travel in futureTravels" :key="travel._id">
               <div class="card my-card">
                 <div class="card-header my-card-header-future">
                   <h2>{{ travel.destination }}</h2>
@@ -111,8 +122,11 @@ export default {
                   <p>Start in <strong>{{ travel.daysUntilStart }}</strong> days</p>
                   <p>Budget <strong>{{ travel.budget }}</strong></p>
                 </div>
-                <!-- Questi dovrebbero uscire dall'ingranaggio -->
-                <!-- <div class="card-footer">
+              </div>
+            </div>
+          </div>
+          <!-- Questi dovrebbero uscire dall'ingranaggio -->
+          <!-- <div class="card-footer">
                   <button class="btn btn-secondary" @click="query('travels', travel._id)">
                     Edit
                   </button>
@@ -120,16 +134,37 @@ export default {
                     Delete
                   </button>
                 </div> -->
+
+          <div class="col-auto m-auto text-center">
+            <RouterLink to="/new-travel" class="btn btn-add fs-1 px-3">
+              <font-awesome-icon :icon="['fas', 'plus']" />
+            </RouterLink>
+          </div>
+
+
+          <!-- Past Travels Section -->
+          <div v-if:="pastTravels">
+            <h3 class="text-start align-self-start">
+              Your past vacays:
+            </h3>
+            <div class="row text-center">
+              <div class="col-12 mb-3" v-for="travel in pastTravels" :key="travel._id">
+                <div class="card my-card">
+                  <div class="card-header my-card-header-past">
+                    <h2>{{ travel.destination }}</h2>
+                    <font-awesome-icon class="fs-3" :icon="['fas', 'gear']" />
+                  </div>
+                  <div class="card-body my-card-body text-start">
+                    <p><em>{{ travel.start_date }} - {{ travel.end_date }}</em></p>
+                    <p>Budget <strong>{{ travel.budget }}</strong></p>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="col-auto m-auto">
-              <RouterLink to="/new-travel" class="btn btn-add">
-                <font-awesome-icon :icon="['fas', 'plus']" />
-              </RouterLink>
             </div>
           </div>
         </div>
       </div>
+
     </section>
   </main>
 </template>
