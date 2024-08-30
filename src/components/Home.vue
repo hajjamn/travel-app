1abc50ea7619faf2245add135d840b60c5d79af6
Jasczlv <ianis.cozlov@gmail.com>
2024-08-30 12:13:48 +0200

    ADD fetch travel data by user ID

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,10 +18,12 @@ export default {
         .then((response) => {
           this.responseData = response.data.data;
           // Separate travels into future and past
-          console.log(this.responseData.travels)
-          this.responseData.travels.forEach(travel => {
+          // console.log(this.responseData.travels)
+          this.responseData.travels.forEach((travel) => {
             const startDate = new Date(travel.start_date);
-            travel.daysUntilStart = Math.ceil((startDate - this.today) / (1000 * 60 * 60 * 24));
+            travel.daysUntilStart = Math.ceil(
+              (startDate - this.today) / (1000 * 60 * 60 * 24)
+            );
 
             // Classify the travel based on its start date
             if (startDate >= this.today) {
@@ -79,7 +81,7 @@ export default {
           },
         })
         .then((response) => {
-          console.log(response.data);
+          // console.log(response.data);
           if (response.data.message) {
             alert(response.data.message);
             this.fetchData();
@@ -103,24 +105,35 @@ export default {
   <main>
     <section class="h-100">
       <div class="container py-5 h-100">
-        <div class="row align-items-center flex-column justify-content-between h-100">
-          <div class="col-auto text-center">
-          </div>
+        <div
+          class="row align-items-center flex-column justify-content-between h-100"
+        >
+          <div class="col-auto text-center"></div>
           <!-- Future Travels Section -->
           <div v-if="futureTravels" class="row text-center">
             <h3 class="text-start align-self-start">
               Here is your planned vacay:
             </h3>
-            <div class="col-12 mb-3" v-for="travel in futureTravels" :key="travel._id">
+            <div
+              class="col-12 mb-3"
+              v-for="travel in futureTravels"
+              :key="travel._id"
+            >
               <div class="card my-card">
                 <div class="card-header my-card-header-future">
                   <h2>{{ travel.destination }}</h2>
                   <font-awesome-icon class="fs-3" :icon="['fas', 'gear']" @click="query('travels', travel._id)" />
                 </div>
                 <div class="card-body my-card-body text-start">
-                  <p><em>{{ travel.start_date }} - {{ travel.end_date }}</em></p>
-                  <p>Start in <strong>{{ travel.daysUntilStart }}</strong> days</p>
-                  <p>Budget <strong>{{ travel.budget }}</strong></p>
+                  <p>
+                    <em>{{ travel.start_date }} - {{ travel.end_date }}</em>
+                  </p>
+                  <p>
+                    Start in <strong>{{ travel.daysUntilStart }}</strong> days
+                  </p>
+                  <p>
+                    Budget <strong>{{ travel.budget }}</strong>
+                  </p>
                 </div>
               </div>
             </div>
@@ -141,22 +154,27 @@ export default {
             </RouterLink>
           </div>
 
-
           <!-- Past Travels Section -->
           <div v-if:="pastTravels">
-            <h3 class="text-start align-self-start">
-              Your past vacays:
-            </h3>
+            <h3 class="text-start align-self-start">Your past vacays:</h3>
             <div class="row text-center">
-              <div class="col-12 mb-3" v-for="travel in pastTravels" :key="travel._id">
+              <div
+                class="col-12 mb-3"
+                v-for="travel in pastTravels"
+                :key="travel._id"
+              >
                 <div class="card my-card">
                   <div class="card-header my-card-header-past">
                     <h2>{{ travel.destination }}</h2>
                     <font-awesome-icon class="fs-3" :icon="['fas', 'gear']" @click="query('travels', travel._id)" />
                   </div>
                   <div class="card-body my-card-body text-start">
-                    <p><em>{{ travel.start_date }} - {{ travel.end_date }}</em></p>
-                    <p>Budget <strong>{{ travel.budget }}</strong></p>
+                    <p>
+                      <em>{{ travel.start_date }} - {{ travel.end_date }}</em>
+                    </p>
+                    <p>
+                      Budget <strong>{{ travel.budget }}</strong>
+                    </p>
                   </div>
                 </div>
               </div>
@@ -164,7 +182,6 @@ export default {
           </div>
         </div>
       </div>
-
     </section>
   </main>
 </template>
